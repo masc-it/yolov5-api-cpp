1a0674543b80cda0d8a50f6d609b4161b6a1b847
onipot <m.sciancalepore20@studenti.uniba.it>
2022-07-15 10:51:33 +0200

    first

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include "detector.h"
+
+/* main */
+
+int main(int argc, char *argv[])
+{
+
+    // TODO load model, classes
+
+    // TODO setup & launch web server
+    return 0;
+}
